cbe63c097c613b9f676cd7988542da9e54441ba9
wangwei83 <wangwei83@cuit.edu.cn>
2025-04-19 22:35:19 +0800

    hah

--- paper-cjh.py
+++ paper-cjh.py
@@ -3,6 +3,9 @@ import pandas as pd
 # plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签\n
 plt.rcParams['font.family'] = 'Times New Roman'  # 设置字体为 Times New Roman
 plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+plt.rcParams['font.size'] = 9  # 设置字体大小为 9 号
+plt.rcParams['axes.titlesize'] = 9  # 设置标题字体大小为 9 号
+
 
 df_cjh = pd.read_csv('./papers-cjh.csv')
 df_cjh_ai = pd.read_csv('./papers-cjh-ai.csv')
@@ -13,14 +16,14 @@ from matplotlib.ticker import MultipleLocator, FormatStrFormatter
 
 fig, ax = plt.subplots(figsize=(10,8))
 
-ax.set_xlabel("Year",fontsize=15)
-ax.set_ylabel("Numbers of papers",fontsize=15)
+ax.set_xlabel("Year",fontsize=9)
+ax.set_ylabel("Numbers of papers",fontsize=9)
 
 ax.plot(df_cjh.year, df_cjh.num,'b-o',label='Total numbers of papers')
 ax.plot(df_cjh_ai.year, df_cjh_ai.num,'r-o',label='Numbers of papers based on AI')
 ax.plot(df_cjh_chinese.year, df_cjh_chinese.num,'g-o',label='Numbers of papers from China')
 
-ax.legend(loc='upper left',fontsize=15)
+ax.legend(loc='upper left',fontsize=9)
 
 
 
@@ -52,4 +55,6 @@ ax.axvline(2020,linewidth=1,linestyle='-.',color='b')
 #             arrowprops=dict(facecolor='black', shrink=0.05),
 #             )
 
-plt.savefig("paper-cjh-ai-chinese-all-gai20250419-times.png", dpi=600, bbox_inches='tight')
+
+
+plt.savefig("paper-cjh-ai-chinese-all-gai20250419-times-size9-all.png", dpi=600, bbox_inches='tight')
